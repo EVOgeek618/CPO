c459054b9e210f34ab94d14e62cb93a26693fbd2
EVOgeek618 <37446747+EVOgeek618@users.noreply.github.com>
2019-10-07 23:30:08 +0300

    Add files via upload

--- Lab_2.sh
+++ Lab_2.sh
@@ -3,11 +3,13 @@ mkdir project
 cd project
 git init
 git clone https://github.com/popcorn-official/popcorn-desktop.git
+chmod -R ugo+rwx popcorn-desktop
 cd popcorn-desktop
 sudo apt install libssl1.0-dev
 sudo apt install nodejs-dev
 sudo apt install node-gyp
 sudo apt install npm
-bower install bower.json
-chmod ugo+rwx ./make_popcorn.sh
-./make_popcorn.sh
+npm install
+sed -i 's/0.33.4/0.35.3/' gulpfile.js
+gulp build
+gulp run
